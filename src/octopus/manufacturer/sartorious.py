aa20aaf1087c52e5666de10930f975d05b67d0e8
Gary Short <gary.short@syngenta.com>
2021-07-07 09:39:01 +0200

    Value coming in a byte string

--- src/octopus/manufacturer/sartorious.py
+++ src/octopus/manufacturer/sartorious.py
@@ -34,13 +34,14 @@ class Sartorious (Machine):
         self.weight = Stream(title = "Weight", type = float, unit = "g")
 
     def start (self):
-        def interpret_weight (result: str):
-            result_unit = result[-3:]
+        def interpret_weight (result):
+            result_decoded = result.decode("utf-8")
+            result_unit = result_decoded[-3:]
 
             if result_unit != "g":
                 raise Exception("Balance units should be set to grams.")
 
-            result_value = float((result[-14:-4]).replace(" ", ""))
+            result_value = float((result_decoded[-14:-4]).replace(" ", ""))
 
             self.weight._push(result_value)
 
